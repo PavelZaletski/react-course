ff1990e9357619c0ee30bae98c84fd12853dfcf1
Pavel Zaletski <pavel.zaletski@viacomcontractor.com>
2018-06-12 15:34:15 +0300

    [task8] storybook updates

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,6 @@ import 'babel-polyfill';
 
 function loadStories() {
   require('../stories/index.js');
-  // You can require as many stories as you need.
 }
 
 configure(loadStories, module);
--- stories/index.js
+++ stories/index.js
@@ -1,34 +1,35 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Footer from '../shared/components/footer';
 import MovieItem from '../shared/components/movieItem';
-import { Provider } from 'react-redux';
 import { getStore } from '../shared/store';
-import { BrowserRouter } from 'react-router-dom';
-import { SearchForm } from '../shared/components/searchForm';
+import SearchForm from '../shared/components/searchForm';
+import MovieNotFound from '../shared/components/movieNotFound';
+import '../client/less/style.less'
+
+const store = getStore({});
 
 storiesOf('Footer', module)
     .add('Footer', () => (
         <Footer />
-    ));
-
-const store = getStore({});
-
-storiesOf('MovieItem', module)
+    ))
     .add('MovieItem', () => (
         <Provider store={store}>
             <BrowserRouter>
                 <MovieItem item={{ title: 'test', genres: ['comedy'], release_date: Date.now() }} />
             </BrowserRouter>
         </Provider>
-    ));
-
-storiesOf('SearchForm', module)
+    ))
     .add('SearchForm', () => (
         <Provider store={store}>
             <BrowserRouter>
                 <SearchForm />
             </BrowserRouter>
         </Provider>
+    ))
+    .add('MovieNotFound', () => (
+         <MovieNotFound />
     ));
